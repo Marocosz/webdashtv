aecee548031b839016e61bc51ac3495f1b1ab83c
Marocosz <ditanixplayer@gmail.com>
2025-04-11 14:02:59 -0300

    11/04 - rail
    1

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Dá permissões de execução (localmente antes de fazer push):
+# chmod +x start.sh
+
 # Inicia o script de commits automáticos em background
 python commit_auto.py &
 
